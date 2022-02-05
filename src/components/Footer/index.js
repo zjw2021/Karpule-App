4ec17622645a5becac730b40737807bd6abe8e1a
Edward Wibowo <wibow9770@gmail.com>
2022-02-05 12:44:06 +0800

    feat: color footer button based on current page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import ExploreIcon from "@mui/icons-material/Explore";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 
+const FooterButton = ({ currentPath, path, icon }) => (
+  <Link to={path}>
+    <IconButton
+      color={currentPath == path ? "primary" : "default"}
+      size="large"
+    >
+      {icon}
+    </IconButton>
+  </Link>
+);
+
 const Footer = () => {
+  const location = useLocation();
   return (
     <AppBar
       color="transparent"
@@ -20,16 +32,16 @@ const Footer = () => {
           justifyContent: "space-evenly",
         }}
       >
-        <Link to="/">
-          <IconButton size="large">
-            <ExploreIcon fontSize="large" />
-          </IconButton>
-        </Link>
-        <Link to="/drive">
-          <IconButton size="large">
-            <DirectionsCarIcon fontSize="large" />
-          </IconButton>
-        </Link>
+        <FooterButton
+          currentPath={location.pathname}
+          path="/"
+          icon={<ExploreIcon fontSize="large" />}
+        />
+        <FooterButton
+          currentPath={location.pathname}
+          path="/drive"
+          icon={<DirectionsCarIcon fontSize="large" />}
+        />
       </Toolbar>
     </AppBar>
   );
