892ff8fe98f4699302c51534f037162e111da560
Edward Wibowo <wibow9770@gmail.com>
2022-02-05 12:19:57 +0800

    feat: improve footer design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { AiOutlineCar, AiOutlineCompass } from 'react-icons/ai'
-import "./footer.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import ExploreIcon from "@mui/icons-material/Explore";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <Link to="/" className="link"><AiOutlineCompass className="icon" /></Link>
-            <Link to="/drive" className="link"><AiOutlineCar className="icon" /></Link>
-        </div>
-    )
-}
+  return (
+    <AppBar
+      color="transparent"
+      position="fixed"
+      sx={{ top: "auto", bottom: 0 }}
+    >
+      <Toolbar
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Link to="/">
+          <IconButton size="large">
+            <ExploreIcon fontSize="large" />
+          </IconButton>
+        </Link>
+        <Link to="/drive">
+          <IconButton size="large">
+            <DirectionsCarIcon fontSize="large" />
+          </IconButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Footer
+export default Footer;
