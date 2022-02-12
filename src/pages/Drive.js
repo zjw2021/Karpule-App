f44fd78dcc5aee8c6343667eb87b32d53e42eb67
Edward Wibowo <wibow9770@gmail.com>
2022-02-12 11:25:50 +0800

    fix: refresh rides on change

--- src/pages/Drive.js
+++ src/pages/Drive.js
@@ -3,18 +3,21 @@ import Layout from "../components/Layout";
 import Menu from "../components/Menu";
 import CreateRide from "../components/CreateRide";
 import DriverRide from "../components/DriverRide";
+import RideContext from "../context/ride/rideContext";
 import UserContext from "../context/users/userContext";
 import Passengers from "../components/Passengers";
 
 const Drive = () => {
   const [rides, setRides] = useState([]);
 
+  const { driverRide } = useContext(RideContext);
+
   const userContext = useContext(UserContext);
   const { user, getDriverRides } = userContext;
 
   useEffect(async () => {
     setRides(await getDriverRides(user.user._id));
-  }, []);
+  }, [driverRide]);
 
   return (
     <Layout>
